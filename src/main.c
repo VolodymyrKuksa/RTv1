ab4f4a9cdea9f6b05586f9092e8f47c096504546
vkuksa <vkuksa@student.unit.ua>
2018-05-14 15:54:44 +0300

    added anti aliasing

--- src/main.c
+++ src/main.c
@@ -27,6 +27,68 @@ void		init_sdl(t_env *env)
 	exit(1);
 }
 
+unsigned int	aa_render(t_env env, t_ray r, t_vec offs, t_vec ppc)
+{
+	t_rgb	col[5];
+	t_rgb	res;
+	t_vec	tmp;
+	double	bias;
+
+
+	bias = 0.3;
+	r.dir = vec_sub(ppc, vec_scale(env.cam.ldir, offs.x + bias));
+	r.dir = vec_add(r.dir, vec_scale(env.cam.updir, offs.y + bias));
+	r.dir = vec_norm(vec_sub(r.dir, r.start));
+	col[0].c = refl_col(r, env, env.refl_depth);
+
+	r.dir = vec_sub(ppc, vec_scale(env.cam.ldir, offs.x - bias));
+	r.dir = vec_add(r.dir, vec_scale(env.cam.updir, offs.y + bias));
+	r.dir = vec_norm(vec_sub(r.dir, r.start));
+	col[1].c = refl_col(r, env, env.refl_depth);
+
+	r.dir = vec_sub(ppc, vec_scale(env.cam.ldir, offs.x - bias));
+	r.dir = vec_add(r.dir, vec_scale(env.cam.updir, offs.y - bias));
+	r.dir = vec_norm(vec_sub(r.dir, r.start));
+	col[2].c = refl_col(r, env, env.refl_depth);
+
+	r.dir = vec_sub(ppc, vec_scale(env.cam.ldir, offs.x + bias));
+	r.dir = vec_add(r.dir, vec_scale(env.cam.updir, offs.y - bias));
+	r.dir = vec_norm(vec_sub(r.dir, r.start));
+	col[3].c = refl_col(r, env, env.refl_depth);
+
+	r.dir = vec_sub(ppc, vec_scale(env.cam.ldir, offs.x));
+	r.dir = vec_add(r.dir, vec_scale(env.cam.updir, offs.y));
+	r.dir = vec_norm(vec_sub(r.dir, r.start));
+	col[4].c = refl_col(r, env, env.refl_depth);
+
+	tmp = vec_new(0, 0, 0);
+	for (int i = 0; i < 4; ++i)
+	{
+		tmp.x += (double)(col[i].bgra[0]) * 0.2;
+		tmp.y += (double)(col[i].bgra[1]) * 0.2;
+		tmp.z += (double)(col[i].bgra[2]) * 0.2;
+//		for (int j = 0; j < 4; ++j)
+//		{
+//			res.bgra[i] += col[j].bgra[i] * 0.125;
+//		}
+	}
+
+//	for (int i = 0; i < 4; ++i)
+//	{
+//		res.bgra[i] += col[4].bgra[i] * 0.5;
+//	}
+
+	tmp.x += (double)(col[4].bgra[0]) * 0.2;
+	tmp.y += (double)(col[4].bgra[1]) * 0.2;
+	tmp.z += (double)(col[4].bgra[2]) * 0.2;
+
+	res.bgra[0] = (unsigned char)tmp.x;
+	res.bgra[1] = (unsigned char)tmp.y;
+	res.bgra[2] = (unsigned char)tmp.z;
+
+	return (res.c);
+}
+
 void		*render_quater(void *data)
 {
 	t_ray			r;
@@ -34,6 +96,7 @@ void		*render_quater(void *data)
 	int				i;
 	int				j;
 	t_thread_data	*td;
+	t_vec			offs;
 
 	td = (t_thread_data*)data;
 	ppc = vec_add(td->env.cam.pos, vec_scale(td->env.cam.dir, PPD));
@@ -44,11 +107,14 @@ void		*render_quater(void *data)
 		j = -1;
 		while (++j < WNDW)
 		{
-			r.dir = vec_sub(ppc, vec_scale(td->env.cam.ldir, (j - WNDW_H)));
-			r.dir = vec_add(r.dir, vec_scale(td->env.cam.updir, (i - WNDH_H)));
-			r.dir = vec_norm(vec_sub(r.dir, r.start));
-			(td->pixels)[i * WNDW + j] =
-				refl_col(r, td->env, td->env.refl_depth);
+//			r.dir = vec_sub(ppc, vec_scale(td->env.cam.ldir, (j - WNDW_H)));
+//			r.dir = vec_add(r.dir, vec_scale(td->env.cam.updir, (i - WNDH_H)));
+//			r.dir = vec_norm(vec_sub(r.dir, r.start));
+//			(td->pixels)[i * WNDW + j] =
+//				refl_col(r, td->env, td->env.refl_depth);
+			offs.x = j - WNDW_H;
+			offs.y = i - WNDH_H;
+			(td->pixels)[i * WNDW + j] = aa_render(td->env, r, offs, ppc);
 		}
 	}
 	free(r.t);
